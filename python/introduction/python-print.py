0c5f3c59b6010baa5da5280804ea67dbc88a2a74
Efe Cini <efecini@gmail.com>
2022-01-04 23:09:13 +0300

    added python-print

---
+++ python/introduction/python-print.py
@@ -0,0 +1,4 @@
+'''Problem Link: https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true'''
+
+if __name__ == '__main__':
+    [print(i, end='') for i in range(1, int(input())+1)]
