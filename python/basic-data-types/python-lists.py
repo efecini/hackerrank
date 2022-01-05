f45e26d6446784950953e0e629fa631b5751c7e6
Efe Cini <efecini@gmail.com>
2022-01-05 16:59:30 +0300

    added python-lists

--- python/basic-data-types/python-lists.py
+++ python/basic-data-types/python-lists.py
@@ -1,15 +1,13 @@
 '''Porblem List: https://www.hackerrank.com/challenges/python-lists/'''
 
 if __name__ == '__main__':
-    lis = 
+    lis = []
     for _ in range(int(input())):
         cmd_args = input().split()
         cmd = cmd_args[0]
         args = cmd_args[1:]
-        if cmd == 'print'
+        if cmd == 'print':
             print(lis)
-        if cmd !="print":
-            cmd += "("+ ",".join(args) +")"
-            eval("l."+cmd)
         else:
-            print(l)
+            cmd += "(" + ",".join(args) + ")"
+            eval("lis."+cmd)
