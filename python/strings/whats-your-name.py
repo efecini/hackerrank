a04cd7bed6bb31b113b0f8a1c4ecda7b1e2f1667
Efe Cini <efecini@gmail.com>
2022-01-10 11:08:14 +0300

    whats-your-name

--- python/strings/whats-your-name.py
+++ python/strings/whats-your-name.py
@@ -1,11 +1,4 @@
-#
-# Complete the 'print_full_name' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts following parameters:
-#  1. STRING first
-#  2. STRING last
-#
+'''Problem Link: https://www.hackerrank.com/challenges/whats-your-name'''
 
 def print_full_name(first, last):
     print(f"Hello {first} {last}! You just delved into python.")
