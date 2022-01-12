62c4b38459b98150da971c768f8be9ce0bc20953
Efe Cini <efecini@gmail.com>
2022-01-12 11:14:04 +0300

    add no-idea

--- python/py-sets/no-idea.py
+++ python/py-sets/no-idea.py
@@ -1,1 +1,11 @@
-first
+'''Problem Link: https://www.hackerrank.com/challenges/py-introduction-to-sets'''
+
+def average(array):
+    s = set(array)
+    return sum(s)/len(s)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
