a22625f719152e1bd90652d0be3c1e1f22dd7a3f
Efe Cini <efecini@gmail.com>
2022-01-13 21:18:02 +0300

    /Users/efecini/Desktop/hackerrank/python/py-sets/py-set-discard-remove-pop.py

---
+++ python/py-sets/py-set-discard-remove-pop.py
@@ -0,0 +1,10 @@
+'''PRoblem Link: https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem
+'''
+
+n = int(input())
+s = set(map(int, input().split()))
+
+for i in range(int(input())):
+    eval('s.{0}({1})'.format(*input().split()+['']))
+
+print(sum(s))
