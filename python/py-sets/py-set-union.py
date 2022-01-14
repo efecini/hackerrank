fbcfb6663153c0349c3b09873d90c511beb77628
Efe Cini <efecini@gmail.com>
2022-01-14 10:53:32 +0300

    add set-union

--- python/py-sets/py-set-union.py
+++ python/py-sets/py-set-union.py
@@ -1,5 +1,7 @@
 '''Problem Link: https://www.hackerrank.com/challenges/py-set-union/problem'''
+_ = input()
 s1 = set(map(int, input().split()))
+_ = input()
 s2 = set(map(int, input().split()))
 
 print(len(s1.union(s2)))
