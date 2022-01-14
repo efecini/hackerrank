c338ecb4f5f4e38eaa08861da228a41334281c6d
Efe Cini <efecini@gmail.com>
2022-01-14 10:56:54 +0300

    add set-intersection

---
+++ python/py-sets/py-set-intersection-operation.py
@@ -0,0 +1,7 @@
+'''https://www.hackerrank.com/challenges/py-set-intersection-operation/problem'''
+_ = input()
+s1 = set(map(int, input().split()))
+_ = input()
+s2 = set(map(int, input().split()))
+
+print(len(s1.intersection(s2)))
