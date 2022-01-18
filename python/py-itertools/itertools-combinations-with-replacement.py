d1d61bc3a57474fe99f9459c7d3b3bbe4a55ad3f
Efe Cini <efecini@gmail.com>
2022-01-18 16:34:17 +0300

    comwithreplace

---
+++ python/py-itertools/itertools-combinations-with-replacement.py
@@ -0,0 +1,9 @@
+'''Problem Link: https://www.hackerrank.com/challenges/itertools-combinations-with-replacement/problem
+'''
+
+from itertools import combinations_with_replacement
+
+s, k = input().split()
+
+for c in combinations_with_replacement(sorted(s), int(k)):
+    print("".join(c))
