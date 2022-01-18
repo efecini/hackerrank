49580b726ce2020e1e7f473910ae23525a16942e
Efe Cini <efecini@gmail.com>
2022-01-18 18:22:43 +0300

    maximize it2

--- python/py-itertools/maximize-it.py
+++ python/py-itertools/maximize-it.py
@@ -5,4 +5,4 @@ from itertools import product
 K,M = map(int,input().split())
 N = (list(map(int, input().split()))[1:] for _ in range(K))
 print(max(map(lambda x: sum(i**2 for i in x)%M, product(*N))))
-cd 
+ 
