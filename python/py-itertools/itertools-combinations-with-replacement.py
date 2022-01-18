da0235da0506421a4b55307777d81fd72c400648
Efe Cini <efecini@gmail.com>
2022-01-18 16:37:50 +0300

    combwithreplace

--- python/py-itertools/itertools-combinations-with-replacement.py
+++ python/py-itertools/itertools-combinations-with-replacement.py
@@ -5,5 +5,5 @@ from itertools import combinations_with_replacement
 
 s, k = input().split()
 
-for c in combinations_with_replacement(sorted(s), int(k)):
+for c in combinations_with_replacement(sorted(s),int(k)):
     print("".join(c))
